ef8222260a8f1e06c09b753a4235f26244c76b3b
Aidan <Aidanm106@gmail.com>
2018-02-12 16:48:59 -0700

    ;

--- HW-4/HW4/sketch.js
+++ HW-4/HW4/sketch.js
@@ -1,10 +1,8 @@
 function setup() {
 
     createCanvas( 900, 400 );
-    background( 'grey' );
 }
 
-function draw(
-  Ellipse( 10, 10, 16, 16 )
-)
-  
+function draw() {
+  Ellipse( 10, 10, 16, 16 );
+}
