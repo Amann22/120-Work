627f2171ec558d723bc74273d377389b4d089886
Aidan <Aidanm106@gmail.com>
2018-02-12 17:44:44 -0700

    ;

--- HW-4/HW4/sketch.js
+++ HW-4/HW4/sketch.js
@@ -8,6 +8,7 @@ function draw() {
     fill('salmon');
     rect( 60, 30, 780, 370, 100, 100, 1, 1 );
 
+
 //mouth
     fill('white');
     ellipse( 400, 350, 300, 60 );
@@ -16,6 +17,9 @@ function draw() {
     fill('black');
     rect( 300, 150, 100, 100, 90, 10 );
     rect( 500, 150, 100, 100, 90, 10);
+
+    noFill();
+    ellipse(420,300,80, 20);
 
 //nose
     fill('black');
