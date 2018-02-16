b2617309a0693791d3d459d276220fba2fb9852c
Aidan <Aidanm106@gmail.com>
2018-02-15 17:18:59 -0700

    ear

--- HW-4/HW4/sketch.js
+++ HW-4/HW4/sketch.js
@@ -6,8 +6,10 @@ function setup() {
 function draw() {
 
     fill('salmon');
+    ellipse(840,200,100,200);
     rect( 60, 30, 780, 370, 100, 100, 1, 1 );
     ellipse(90,200, 100,200);
+
 
     line(120,110, 720,110);
     line(135,130, 720,130);
