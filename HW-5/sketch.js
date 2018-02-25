3876ed79f6def56c2141e57cae5a111f167ae41d
Aidan <Aidanm106@gmail.com>
2018-02-25 14:30:42 -0700

    s

--- HW-5/sketch.js
+++ HW-5/sketch.js
@@ -21,8 +21,6 @@ function draw(){
   ellipse(mouseX,mouseY+200,40,40);
   ellipse(mouseX,mouseY+300,40,40);
   ellipse(mouseX,mouseY+400,40,40);
-  ellipse(mouseX,mosueY-100,40,40);
-  ellipse(mouseX,mouseY-200,40,40);
-  ellipse(mouseX,mouseY-300,40,40);
+  
 
 }
