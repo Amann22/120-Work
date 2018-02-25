0cd0a0e501b9a4cb96c28b7ce1d0bb2cf2dc10b4
Aidan <Aidanm106@gmail.com>
2018-02-25 13:46:30 -0700

    its alive

--- HW-5/sketch.js
+++ HW-5/sketch.js
@@ -1,15 +1,14 @@
+var circlex = 0;
 
 function setup(){
-
-  createCanvas( 600,600);
-
+  createCanvas( 600,400);
 }
+
 function draw(){
+  background('salmon');
+  fill('red');
+  ellipse(circlex,0,50,50);
+  ellipse(circlex,400,50,50);
 
-background('salmon');
-
-fill('red');
-ellipse(0,0,50,50);
-ellipse(0,600,50,50);
-
+circlex = circlex + 1;
 }
