70b91a4090b63f80317ebb732bb96b5ca0e8e298
Aidan <Aidanm106@gmail.com>
2018-02-25 14:00:22 -0700

    clean

--- HW-5/sketch.js
+++ HW-5/sketch.js
@@ -1,5 +1,4 @@
 var circlex = 0;
-var circleY = 0;
 
 function setup(){
   createCanvas( 600,400);
@@ -15,7 +14,5 @@ function draw(){
   ellipse(circlex,200,50,50);
   ellipse(circlex,300,50,50);
   circlex = circlex + 1;
-  circleY = circleY + 1;
-  //red circles vertical
 
 }
