a34a81948cc4889a6c7e02c07a733445bf43af24
Aidan <Aidanm106@gmail.com>
2018-02-26 13:48:47 -0700

    p

--- HW-6/sketch.js
+++ HW-6/sketch.js
@@ -5,8 +5,10 @@ function setup(){
   createCanvas(600,400);
   background('salmon');
 }
-function draw(){
-line(rand,rand);
 
+function draw(){
+
+  let circle_size = random( 10, width-10 );
+      ellipse( width/2, height/2, circle_size );
 
 }
