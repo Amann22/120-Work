0dedc29090fed7535295c5889ad1c6f7c89d371d
Aidan <Aidanm106@gmail.com>
2018-02-26 19:52:43 -0700

    s

--- HW-6/sketch.js
+++ HW-6/sketch.js
@@ -13,10 +13,6 @@ let colry= floor( random(256) );
 fill(colr,colrx,colry);
   let circle_size = random( 10, width-10 );
       ellipse( width/2, height/2, circle_size );
-      //backround
-      let red = floor( random(256) );
-          let green = floor( random(256) );
-          let blue = floor( random(256) );
-          background( red, green, blue );
+      
 
 }
