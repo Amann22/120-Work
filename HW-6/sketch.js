666c27226dce92454efb7e1247fefa5c4d21fcfb
Aidan <Aidanm106@gmail.com>
2018-02-26 20:11:33 -0700

    s

--- HW-6/sketch.js
+++ HW-6/sketch.js
@@ -2,16 +2,18 @@
 function setup(){
   createCanvas(600,600);
   background('salmon');
-  frameRate(10);
+  frameRate(20);
 }
 
 let circleX=10;
 let circleY=590;
 
 function draw(){
+  //color changing circles
 let colr = floor( random(256) );
 let colrx= floor( random(256) );
 let colry= floor( random(256) );
+
 fill(colr,colrx,colry);
   let circle_size = random( 10, width-10 );
       ellipse( width/2, height/2, circle_size );
@@ -19,6 +21,13 @@ fill(colr,colrx,colry);
     fill('violet');
     noStroke();
     ellipse( circleX, 150, 30 );
+
+    circleX = circleX + 5;
+    circleX = circleX % width;
+
+    fill('violet');
+    noStroke();
+    ellipse( circleX, 250, 30 );
 
     circleX = circleX + 5;
     circleX = circleX % width;
