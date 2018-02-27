20fa0987bf587f0f1bf3e66e44e3072b37a03632
Aidan <Aidanm106@gmail.com>
2018-02-26 20:00:38 -0700

    s

--- HW-6/sketch.js
+++ HW-6/sketch.js
@@ -5,6 +5,7 @@ function setup(){
   frameRate(10);
 }
 
+let circleX=10;
 
 function draw(){
 let colr = floor( random(256) );
@@ -13,6 +14,12 @@ let colry= floor( random(256) );
 fill(colr,colrx,colry);
   let circle_size = random( 10, width-10 );
       ellipse( width/2, height/2, circle_size );
-      
+//
+    fill(255);
+    noStroke();
+    ellipse( circleX, 140, 30 );
+
+    circleX = circleX + 5;
+    circleX = circleX % width;
 
 }
