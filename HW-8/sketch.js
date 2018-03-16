25cd6cb77681efbdd10ae328ca03256804f18496
Aidan <Aidanm106@gmail.com>
2018-03-16 14:18:09 -0600

    s

--- HW-8/sketch.js
+++ HW-8/sketch.js
@@ -4,20 +4,29 @@ function setup() {
 }
 
 function draw() {
-	for (let y = 30; y <= height; y += 16) {
+  for (let y = 30; y <= height; y += 16) {
 		for (let x = 12; x <= width; x += 16) {
+			fill('yellow');
+      noStroke();
+			ellipse(x + y, y, 16 - y/10.0, 16 - y/10.0);
+		}
+	}
+	for (let y = 10; y <= height; y += 16) {
+		for (let x = -200; x <= width; x += 16) {
 			fill('teal');
       noStroke();
 			ellipse(x + y, y, 16 - y/10.0, 16 - y/10.0);
 		}
 	}
   for (let y = 10; y <= height; y += 19) {
-		for (let x = -200; x <= width; x += 19) {
+		for (let x = -1000; x <= width; x += 19) {
 			noFill();
       stroke(5);
 			ellipse(x + y, y, 16 - y/10.0, 16 - y/10.0);
 		}
 	}
+
+
 
 
 }
