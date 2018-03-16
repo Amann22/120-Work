e1958d6e45862f23928d73575e61a2f16c5ac082
Aidan <Aidanm106@gmail.com>
2018-03-16 13:58:17 -0600

    s

--- HW-8/sketch.js
+++ HW-8/sketch.js
@@ -4,7 +4,7 @@ function setup() {
 
 function draw() {
 	background(0);
-	for (let y = 32; y <= height; y += 8) {
+	for (let y = 50; y <= height; y += 8) {
 		for (let x = 12; x <= width; x += 15) {
 			fill(255);
 			ellipse(x + y, y, 16 - y/10.0, 16 - y/10.0);
