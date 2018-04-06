b85ea709feb6f6b7245fe7979e1e3aa196976644
Aidan <Aidanm106@gmail.com>
2018-04-06 13:24:21 -0600

    s

--- HW-10/sketch.js
+++ HW-10/sketch.js
@@ -37,6 +37,6 @@ function draw() {
 }
 
 function mousePressed() {
-    ball.scale_x = map(mouseX, 0, width, 0.5, 10);
-    ball.scale_y = map(mouseY, 0, height, 0.5, 10);
+    ball.scale_x = map(mouseX, 0, width, 0.5, array[1]);
+    ball.scale_y = map(mouseY, 0, height, 0.5, array[1]);
 }
