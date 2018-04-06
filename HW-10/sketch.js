6adee0734343c612469d4b11784e6ff0c59eea5f
Aidan <Aidanm106@gmail.com>
2018-04-06 13:22:52 -0600

    save

---
+++ HW-10/sketch.js
@@ -0,0 +1,42 @@
+var array= [50,10,1];
+
+let ball = {};
+ball.width = array[0];
+ball.x = array[1];
+ball.y = array[1];
+ball.delta_x = array[2];
+ball.delta_y = array[2];
+ball.scale_x = array[2];
+ball.scale_y = array[2];
+
+function setup() {
+    createCanvas(windowWidth, 400);
+    background( 0);
+    frameRate(100);
+}
+
+function draw() {
+  if( mouseIsPressed ) {
+        ball.width++;
+    }
+  noStroke();
+
+    ball.x += ball.delta_x * ball.scale_x;
+    ball.y += ball.delta_y * ball.scale_y;
+
+
+    if (ball.x >= width || ball.x <= 0) {
+        ball.delta_x = -1 * ball.delta_x;
+    }
+    if (ball.y >= height || ball.y <= 0) {
+        ball.delta_y = -1 * ball.delta_y;
+    }
+
+    fill( random(255), random(255), random(255) );
+    ellipse(ball.x, ball.y, ball.width, ball.width);
+}
+
+function mousePressed() {
+    ball.scale_x = map(mouseX, 0, width, 0.5, 10);
+    ball.scale_y = map(mouseY, 0, height, 0.5, 10);
+}
