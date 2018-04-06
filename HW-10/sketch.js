a6d06d77a5de514642a63e734861bf9220869896
Aidan <Aidanm106@gmail.com>
2018-04-06 13:45:08 -0600

    s

--- HW-10/sketch.js
+++ HW-10/sketch.js
@@ -19,24 +19,29 @@ function draw() {
   if( mouseIsPressed ) {
         ball.width++;
     }
-  noStroke();
-
-    ball.x += ball.delta_x * ball.scale_x;
-    ball.y += ball.delta_y * ball.scale_y;
-
-
-    if (ball.x >= width || ball.x <= 0) {
-        ball.delta_x = -1 * ball.delta_x;
-    }
-    if (ball.y >= height || ball.y <= 0) {
-        ball.delta_y = -1 * ball.delta_y;
-    }
-
-    fill( random(255), random(255), random(255) );
-    ellipse(ball.x, ball.y, ball.width, ball.width);
 }
 
 function mousePressed() {
     ball.scale_x = map(mouseX, 0, width, 0.5, array[1]);
     ball.scale_y = map(mouseY, 0, height, 0.5, array[1]);
+}
+
+function move () {
+  ball.x += ball.delta_x * ball.scale_x;
+  ball.y += ball.delta_y * ball.scale_y;
+}
+function bounce() {
+  if (ball.x >= width || ball.x <= 0) {
+      ball.delta_x = -1 * ball.delta_x;
+  }
+  if (ball.y >= height || ball.y <= 0) {
+      ball.delta_y = -1 * ball.delta_y;
+}
+}
+function display() {
+  fill( random(255), random(255), random(255) );
+  ellipse(ball.x, ball.y, ball.width, ball.width);
+  noStroke();
+  fill( random(255), random(255), random(255) );
+  ellipse(ball.x, ball.y, ball.width, ball.width);
 }
